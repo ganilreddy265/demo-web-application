53e1cf0986f6139a4d952222e34fb7e3b6b30656
Anil <101619758+Anil14041999@users.noreply.github.com>
2025-10-19 13:15:12 +0530

    five commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
 # tests/test_app.py
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
+
 
 def test_home_page():
     tester = app.test_client()
